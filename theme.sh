e5fe9ff8120bec3e4cc982a64218a6ccf6951e9f
mokka-official <sakuraiyukino_official@outlook.com>
2026-01-22 17:17:34 +0800

    最小化

---
+++ theme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $HOME
+rm -f $HOME/.hushlogin
+touch $HOME/.hushlogin
+cat >$PREFIX/etc/apt/sources.list <<'EOF'
+deb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main
+EOF
+apt update
+yes | apt upgrade
+mkdir $HOME/.img
+ln -s $PREFIX $HOME
+cp $HOME/heretic_demon/colors.properties $HOME/.termux/
+cp $HOME/heretic_demon/termux.properties $HOME/.termux/
+cp $HOME/heretic_demon/font/ComicShannsMonoNerdFontMono-Bold.otf $HOME/.termux/font.ttf
+cp $HOME/heretic_demon/wallpaper/back.jpg $HOME/.img/
+cat >$HOME/.bashrc <<'EOF'
+exec fish
+EOF
